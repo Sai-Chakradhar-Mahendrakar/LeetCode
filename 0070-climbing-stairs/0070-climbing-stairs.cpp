a22cfb9547adcb4e43a46b142ea43d9f3890a3eb
Sai Chakradhar Rao Mahendrakar <114484848+Sai-Chakradhar-Mahendrakar@users.noreply.github.com>
2024-05-12 09:54:45 +0530

    Time: 3 ms (28.78%), Space: 7.3 MB (31.84%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> memo;
+    int dp(int n){
+        if(n==0 or n==1 or n==2){
+            return n;
+        }
+        if(memo[n]!=-1){
+            return memo[n];
+        }
+        return memo[n]=dp(n-1)+dp(n-2);
+    }
+    int climbStairs(int n) {
+        memo.resize(n+1,-1);
+        return dp(n);
+    }
+};
