df1686424de474c6382f1c7c53b142013736f0a9
Sai Chakradhar Rao Mahendrakar <114484848+Sai-Chakradhar-Mahendrakar@users.noreply.github.com>
2024-06-14 21:41:11 +0530

    Time: 353 ms (52.27%), Space: 174.3 MB (59.41%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,83 @@
+class Node {
+public:
+    int k;
+    int val;
+    Node* prev;
+    Node* next;
+    
+    Node(int key, int value) {
+        k = key;
+        val = value;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+class LRUCache {
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+        
+        left = new Node(0, 0);
+        right = new Node(0, 0);
+        
+        left->next = right;
+        right->prev = left;
+    }
+    
+    int get(int key) {
+        if (cache.find(key) != cache.end()) {
+            remove(cache[key]);
+            insert(cache[key]);
+            return cache[key]->val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            remove(cache[key]);
+            
+            // Free allocated memory for the removed node
+            delete cache[key];
+        }
+        cache[key] = new Node(key, value);
+        insert(cache[key]);
+        
+        if (cache.size() > cap) {
+            // remove from list & delete LRU from map
+            Node* lru = left->next;
+            remove(lru);
+            cache.erase(lru->k);
+            
+            // Free allocated memory for the removed node
+            delete lru;
+        }
+    }
+private:
+    int cap;
+    unordered_map<int, Node*> cache; // {key -> node}
+    Node* left;
+    Node* right;
+    
+    // remove node from list
+    void remove(Node* node) {
+        Node* prev = node->prev;
+        Node* next = node->next;
+        
+        prev->next = next;
+        next->prev = prev;
+    }
+    
+    // insert node at right
+    void insert(Node* node) {
+        Node* prev = right->prev;
+        Node* next = right;
+        
+        prev->next = node;
+        next->prev = node;
+        
+        node->prev = prev;
+        node->next = next;
+    }
+};
