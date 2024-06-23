5cb2c8078bdc211bb6d6a0eb6b3fa4fcc999e780
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-06-23 10:35:39 +0530

    Time: 156 ms (73.97%), Space: 29.1 MB (30.06%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void primeSieve(int sieve[], int N) {
+        // Mark 1 and 0 as not prime
+        sieve[0] = 0;
+        sieve[1] = 0;
+
+        // Initially mark all numbers as prime
+        for (int i = 2; i <= N; i++) {
+            sieve[i] = 1;
+        }
+
+        // Start from 2 and mark all multiples of given i (prime) as not prime
+        for (long long i = 2; i * i <= N; i++) {
+            if (sieve[i]) {
+                for (long long j = i * i; j <= N; j += i) {
+                    // Marking j as not prime
+                    sieve[j] = 0;
+                }
+            }
+        }
+    }
+
+    int countPrimes(int n) {
+        // Handle the case where n is less than 2
+        if (n < 2) {
+            return 0;
+        }
+
+        int sieve[n + 1];  // Create sieve array
+        primeSieve(sieve, n);
+
+        int cnt = 0;
+        for (int i = 2; i < n; i++) {
+            if (sieve[i]) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
