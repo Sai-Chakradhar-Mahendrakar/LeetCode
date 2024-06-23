10bef17ea4b2de407bc4bc731ea245380d12b51f
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-06-23 09:23:36 +0530

    Time: 0 ms (100%), Space: 7.9 MB (93.45%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+
+        long long N = n;
+        while(N<0){
+            x = 1/x;
+            N = -N;
+        }
+
+        double res=1;
+        while(N){
+            if(N&1){
+                res*=x;
+            }
+            x*=x;
+            N/=2;
+        }
+        return res;
+    }
+};
