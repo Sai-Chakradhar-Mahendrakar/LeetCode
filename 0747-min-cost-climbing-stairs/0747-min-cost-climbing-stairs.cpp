e21fba81ed50690a2d07294df8a9a76544c92670
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-07-04 21:30:48 +0530

    Time: 3 ms (79.95%), Space: 16.4 MB (19.14%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> memo;
+    int dp(vector<int>& cost, int idx){
+        if(idx>=cost.size()){
+            return 0;
+        }
+
+        if(memo[idx]!=-1){
+            return memo[idx];
+        }
+
+        int takeOneStep = dp(cost,idx+1);
+        int takeTwoSteps = dp(cost,idx+2);
+
+        return memo[idx]=min(takeOneStep,takeTwoSteps)+cost[idx];
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        memo.resize(cost.size()+1,-1);
+        return min(dp(cost,0),dp(cost,1));
+    }
+};
