1a84b291524245476e909ea3b8d26a2c85c4ea6f
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-07-07 12:25:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> memo;
+    vector<vector<int>> memo1;
+    int isPalindrome(string& s, int i, int j){
+        if(i==j) return true;
+        if(i==j-1) return (s[i]==s[j]);
+        if(memo1[i][j]!=-1) return memo1[i][j];
+        return memo1[i][j]=isPalindrome(s,i+1,j-1) and (s[i]==s[j]);
+    }
+
+    int dp(string &s, int idx){
+        if(idx==s.size()){
+            return 0;
+        }
+
+        if(memo[idx]!=-1){
+            return memo[idx];
+        }
+
+        int ans=INT_MAX;
+        for(int j=idx;j<s.size();j++){
+            if(isPalindrome(s, idx, j)){
+                ans = min(ans, dp(s,j+1));
+            }
+        }
+
+        ans+=1;
+        return ans;
+    }
+
+    int minCut(string s) {
+        memo.resize(s.size(),-1);  
+        memo1.resize(s.size(), vector<int>(s.size(),-1)); 
+        return dp(s, 0)-1;
+    }
+};
