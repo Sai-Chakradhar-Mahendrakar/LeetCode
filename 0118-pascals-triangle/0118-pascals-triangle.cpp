9030cfd7121c7d6e0fc3f5d8e03f290aedd431df
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-07-08 19:14:53 +0530

    Time: 3 ms (36.82%), Space: 7.9 MB (47.65%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans(numRows);
+
+        // ReSizing
+        for(int i=0;i<numRows;i++){
+            ans[i].resize(i+1);
+            ans[i][0] = 1;
+            ans[i][i] = 1;
+
+            // Dyanmic Programming
+            for (int j = 1; j < i; ++j) {
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
+        }
+
+        return ans;
+    }
+};
