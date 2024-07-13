bd73c4576e89380d88fd772b0f92fb786f3dc7f1
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-07-13 18:58:13 +0530

    Time: 3 ms (84.18%), Space: 16.3 MB (21.17%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+
+        return bfs(grid, r, c);
+    }
+
+private:
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0 , 1, -1};
+
+    int bfs(vector<vector<int>>& grid, int r, int c){
+        queue<pair<int,int>> q;
+        int ans=-1;
+        int fresh=0;
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }else if(grid[i][j]==1){
+                    fresh++;
+                }
+            }
+        }
+
+        q.push({-1, -1});
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            if(row==-1){
+                ans++;
+                if(!q.empty()){
+                    q.push({-1,-1});
+                }
+            }else{
+                for(int i=0;i<4;i++){
+                    int nx = row + dx[i];
+                    int ny = col + dy[i];
+
+                    if(nx<0 or nx>=r or ny<0 or ny>=c){
+                        continue;
+                    } 
+
+                    if(grid[nx][ny]==1){
+                        fresh--;
+                        grid[nx][ny]=2;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+        }
+
+        if(fresh==0){
+            return ans;
+        }
+        
+        return -1;
+    }
+};
