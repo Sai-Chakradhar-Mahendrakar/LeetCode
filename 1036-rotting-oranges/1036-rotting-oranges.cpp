367874de45c0d02986e31b0cd3b7fec0daa18b16
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-07-13 19:00:41 +0530

    Time: 0 ms (100%), Space: 16.1 MB (31.66%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -9,18 +9,18 @@ public:
 
 private:
     int dx[4] = {1, -1, 0, 0};
-    int dy[4] = {0, 0 , 1, -1};
+    int dy[4] = {0, 0, 1, -1};
 
-    int bfs(vector<vector<int>>& grid, int r, int c){
-        queue<pair<int,int>> q;
-        int ans=-1;
-        int fresh=0;
+    int bfs(vector<vector<int>>& grid, int r, int c) {
+        queue<pair<int, int>> q;
+        int ans = -1;
+        int fresh = 0;
 
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
-                }else if(grid[i][j]==1){
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                } else if (grid[i][j] == 1) {
                     fresh++;
                 }
             }
@@ -28,38 +28,36 @@ private:
 
         q.push({-1, -1});
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
 
-            if(row==-1){
+            if (row == -1) {
                 ans++;
-                if(!q.empty()){
-                    q.push({-1,-1});
+                if (!q.empty()) {
+                    q.push({-1, -1});
                 }
-            }else{
-                for(int i=0;i<4;i++){
+            } else {
+                for (int i = 0; i < 4; i++) {
                     int nx = row + dx[i];
                     int ny = col + dy[i];
 
-                    if(nx<0 or nx>=r or ny<0 or ny>=c){
+                    if (nx < 0 or nx >= r or ny < 0 or ny >= c) {
                         continue;
-                    } 
-
-                    if(grid[nx][ny]==1){
+                    } else if (grid[nx][ny] == 1) {
                         fresh--;
-                        grid[nx][ny]=2;
+                        grid[nx][ny] = 2;
                         q.push({nx, ny});
                     }
                 }
             }
         }
 
-        if(fresh==0){
+        if (fresh == 0) {
             return ans;
         }
-        
+
         return -1;
     }
 };
