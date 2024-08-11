eb523c68139d6a4cfe4e0ee65e14fb1da0ec27a7
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-11 14:53:45 +0530

    Time: 5 ms (78.29%), Space: 21.4 MB (37.85%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int i=1;
         for(int j=1;j<nums.size();j++){
-            if(nums[j]!=nums[j-1]){
+            if(nums[j]!=nums[i-1]){
                 nums[i]=nums[j];
                 i++;
             }
