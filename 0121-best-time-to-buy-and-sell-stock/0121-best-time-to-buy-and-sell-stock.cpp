bddcae5597f0940e60b9b7bf692c1adf46e55d37
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-13 23:06:42 +0530

    Time: 92 ms (30.16%), Space: 96.1 MB (44.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxS=0, l=0, r=0;
+        int maxProfit=0,l=0,r=0;
         while(r<prices.size()){
             if(prices[l]<prices[r]){
-                maxS = max(maxS, prices[r]-prices[l]);
+                maxProfit = max(maxProfit, prices[r]-prices[l]);
             }
             else{
                 l=r;
             }
             r++;
         }
-        return maxS;
+        return maxProfit;
     }
 };
