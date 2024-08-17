ac80bf176658c177deea7b69dc07f2760b6608c6
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-17 10:45:46 +0530

    Time: 66 ms (53.01%), Space: 27.5 MB (68.81%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        std::sort(nums.begin(),nums.end());
-        int n = nums.size();
+        int n=nums.size();
         vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+
         for(int i=0;i<n-2;i++){
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
             int j=i+1;
             int k=n-1;
-            int current_sum=0;
+            
             while(j<k){
-                current_sum=nums[i]+nums[j]+nums[k];
-                if(current_sum==0){
-                    cout<<nums[i]<<nums[j]<<nums[k]<<endl;
-                    res.emplace_back(vector<int>{nums[i], nums[j], nums[k]});
+                int curSum= nums[i]+nums[j]+nums[k];
+                if(curSum==0){
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    
                     while (j < k && nums[j] == nums[j + 1]) j++;
                     while (j < k && nums[k] == nums[k - 1]) k--;
                     j++;
                     k--;
                 }
-                else if(current_sum>0){
+                else if(curSum>0){
                     k--;
                 }
                 else{
@@ -27,6 +31,6 @@ public:
                 }
             }
         }
-    return res;
+        return res;
     }
 };
