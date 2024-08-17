8374dfabda2fa2ac7af8b079470db069ac1e600b
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-17 19:45:26 +0530

    Time: 9 ms (87.47%), Space: 20.7 MB (33.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int res = n;
-        for(int i=0;i<n;i++){
-            res ^= i ^ nums[i];
+        int res=nums.size();
+        for(int i=0;i<nums.size();i++){
+            res = res^i^nums[i];
         }
         return res;
     }
