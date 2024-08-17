d93a6a321f99b15e71efbf737e1d87cec6887940
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-17 10:15:59 +0530

    Time: 3 ms (46.7%), Space: 7.8 MB (14.06%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.size() and j < t.size()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.size();
+    }
+};
