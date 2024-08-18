f0871edd57c4fbb7fed4abdbb53193a83d976e33
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-18 14:05:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int reverse(int x) {
+        int temp=x;
         int rev=0;
-        while(x!=0){
-            int temp = x%10;
-            x /= 10;
-            if(rev > INT_MAX/10 or (rev ==INT_MAX/10 and temp>7)){
-                return 0;
-            }
-            if(rev < INT_MIN / 10 or (rev == INT_MIN / 10 and temp < -8)){
-                return 0;
-            }
-            rev = rev*10 + temp;
+
+        bool neg=false;
+
+        if(x<0){
+            neg=true;
+            temp*=-1;
         }
-        return rev;
+
+        while(temp>0){
+            int lastDigit=temp%10;
+            temp = temp/10;
+            rev = rev*10 + lastDigit;
+        }
+
+        return neg ? rev*-1 : rev;
     }
 };
