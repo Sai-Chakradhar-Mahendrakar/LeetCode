4082562dac2f115b6d07046524a177da03c38c05
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-18 14:16:32 +0530

    Time: 3 ms (93.3%), Space: 8.3 MB (67.37%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0){
-            return 0;
+            return false;
         }
 
         int temp = x;
         int rev = 0;
         while(temp>0){
             int lastDigit=temp%10;
+            
+            if (rev > (INT_MAX - lastDigit) / 10) {
+                return false; // Overflow, so it can't be a palindrome
+            }
+            
             temp/=10;
             rev = rev*10 + lastDigit;
         }
 
-        return x==rev ? true : false;
+        return x==rev;
     }
 };
