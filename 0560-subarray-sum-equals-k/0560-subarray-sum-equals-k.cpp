153ce69ad6428edcf8007d31b7a97c645cecd8e0
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-19 08:29:10 +0530

    Time: 63 ms (75.89%), Space: 50.9 MB (23.02%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+
+        int sum = 0;
+        int cnt = 0;
+        mp[sum] = 1;
+
+        for (auto x : nums) {
+            sum += x;
+            cnt += mp[sum - k];
+            mp[sum]++;
+        }
+
+        return cnt;
+    }
+};
