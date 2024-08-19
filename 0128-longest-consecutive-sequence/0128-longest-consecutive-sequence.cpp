ed4fa4b599e7d50221af955a06c2c102118de7cb
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-19 07:47:59 +0530

    Time: 116 ms (64.53%), Space: 74.6 MB (50.97%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(),nums.end());
-        
-        int lar = 0;
-        
-        for(auto i:nums){
-            if(s.find(i-1)==s.end()){
+        // ios::sync_with_stdio(0);
+        // cin.tie(0);
+        // cout.tie(0);
+
+        int n = nums.size();
+        unordered_set<int> s(nums.begin(), nums.end());
+
+        int longestBand = 0;
+
+        for(int element: s){
+            if(s.find(element-1)==s.end()){
+                int next = element+1;
                 int cnt=1;
-                while(s.find(i+1)!=s.end()){
-                    i++;
+                while(s.find(next)!=s.end()){
+                    next++;
                     cnt++;
                 }
-                lar = max(lar,cnt);
+                longestBand = max(longestBand, cnt);
             }
         }
-        return lar;
+        return longestBand;
     }
 };
