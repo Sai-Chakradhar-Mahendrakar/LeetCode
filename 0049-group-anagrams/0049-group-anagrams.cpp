3b14a64d6e79708a51a826fd22d81043ed31c3ea
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-19 22:05:35 +0530

    Time: 34 ms (28.83%), Space: 25.2 MB (31.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
-        unordered_map<string, vector<string>> m;
-        for(auto str:strs){
-            auto key = str;
-            sort(key.begin(),key.end());
-            m[key].emplace_back(str);
+        unordered_map<string, vector<string>> mp;
+
+        for(auto str: strs){
+            auto org = str;
+            sort(str.begin(), str.end());
+            mp[str].push_back(org);
         }
-        
-        for(auto pair:m){
-            res.emplace_back(pair.second);
+
+        for(auto pair:mp){
+            res.push_back(pair.second);
         }
-        
+
         return res;
     }
 };
