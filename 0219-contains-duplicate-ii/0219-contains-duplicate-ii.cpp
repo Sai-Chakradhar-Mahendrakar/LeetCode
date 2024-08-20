ff78a3f388aef9454576c7750f9bdd58334399ff
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-20 21:33:22 +0530

    Time: 106 ms (92.21%), Space: 81.2 MB (46.93%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+
+        for(int i=0;i<n;i++){
+            if(mp.count(nums[i])){
+                if(abs(i-mp[nums[i]])<=k){
+                    return true;
+                }
+            }
+            mp[nums[i]]=i;
+        }
+        return false;
+    }
+};
