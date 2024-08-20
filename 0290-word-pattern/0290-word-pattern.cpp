4c18a8b9bf9c84caccbd3a75d70ac884aa4d45b1
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-20 21:51:07 +0530

    Time: 3 ms (39.07%), Space: 8 MB (50.36%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        stringstream ss(s);
+        string word;
+        unordered_map<string, char>mps;
+        unordered_map<char, char>mpp;
+
+        string sstr="";
+        string pstr="";
+
+        char ch = 'a';
+
+        while(ss >> word){
+            if(mps.find(word)==mps.end()){
+                mps[word]=ch++;
+            }
+            sstr+=mps[word];
+        }
+
+        ch = 'a';
+
+        for(auto it:pattern){
+            if(mpp.find(it)==mpp.end()){
+                mpp[it]=ch++;
+            }
+            pstr+=mpp[it];
+        }
+
+        if(sstr==pstr){
+            return true;
+        }
+        
+        return false;
+    }
+};
