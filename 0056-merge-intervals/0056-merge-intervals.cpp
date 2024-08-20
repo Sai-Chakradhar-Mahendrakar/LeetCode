8ab6f214a39dda68fe3b40bd8200c3bb83b6985d
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-20 13:20:52 +0530

    Time: 30 ms (19.29%), Space: 23 MB (31.93%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        if(n<=1){
+            return intervals;
+        }
+
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        for(int i=1;i<n;i++){
+            auto& lastInterval = res.back();
+
+            if(lastInterval[1]>=intervals[i][0]){
+                lastInterval[1] = max(lastInterval[1], intervals[i][1]);
+            }
+            else{
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+    }
+};
