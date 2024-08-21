fca1aaf14bfef16f946e2324acf6d68c1a3b14ab
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-21 14:55:09 +0530

    Time: 3 ms (42.64%), Space: 8.1 MB (17.03%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         stack<char> st;
         int ans=0;
 
