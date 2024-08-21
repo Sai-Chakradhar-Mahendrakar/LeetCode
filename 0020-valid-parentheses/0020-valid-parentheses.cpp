5da0dcbedeaaa146b8ae79e7ddeddf2477226d21
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-21 21:43:24 +0530

    Time: 0 ms (100%), Space: 7.7 MB (75.87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isValid(string s) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         stack<char> st;
         for(auto x: s){
             if(st.empty()){
