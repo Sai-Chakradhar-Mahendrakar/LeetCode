7d6957f1d63b6280ccf1ddc0d4effe8fa01d0ee6
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-23 09:37:40 +0530

    Time: 3 ms (40.83%), Space: 7.8 MB (26.8%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int cnt=0;
+        int ans=0;
+        
+        for(auto ch: s){
+            if(ch=='('){
+                cnt++;
+                ans=max(ans, cnt);
+            }
+            else if(ch==')'){
+                cnt--;
+            }
+        }
+        return ans;
+    }
+};
