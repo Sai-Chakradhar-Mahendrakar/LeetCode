b43e16152b34059014bd7e2d421a8d87a5c58719
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-23 09:38:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (15.85%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxDepth(string s) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int cnt=0;
         int ans=0;
         
