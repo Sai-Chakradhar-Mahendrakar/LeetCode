1dbc4defc7970ced38aa0a6e5ad8ea13001faa8e
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-23 23:04:27 +0530

    Time: 177 ms (36.37%), Space: 8 MB (91.15%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int beautySum(string s) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int ans=0;
 
         for(int i=0;i<s.size();i++){
