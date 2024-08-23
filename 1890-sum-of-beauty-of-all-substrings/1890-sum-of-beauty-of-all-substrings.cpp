9c5c3ec040ffcd54997cefe8ae82c8de3b67aa18
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-23 23:02:36 +0530

    Time: 181 ms (35.35%), Space: 7.6 MB (96.81%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int ans=0;
+
+        for(int i=0;i<s.size();i++){
+            int mp[26]={0};
+            for(int j=i;j<s.size();j++){
+                mp[s[j]-'a']++;
+
+                int minFreq = INT_MAX;
+                int maxFreq = INT_MIN;
+
+                for(int k=0;k<26;k++){
+                    if(mp[k]>0){
+                        maxFreq = max(maxFreq, mp[k]);
+                        minFreq = min(minFreq, mp[k]);
+                    }
+                }
+                ans+=(maxFreq-minFreq);
+            }
+        }
+        return ans;
+    }
+};
