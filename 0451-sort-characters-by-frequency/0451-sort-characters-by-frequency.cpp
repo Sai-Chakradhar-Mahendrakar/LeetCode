7e25be706dd725e3d4eb8720dfbb02c44a84c933
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-23 09:24:28 +0530

    Time: 10 ms (65.39%), Space: 10.5 MB (86.66%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        string ans="";
+        vector<pair<char, int>> v(127);
+
+        for(auto ch: s){
+            int freq = v[ch].second;
+            v[ch] = {ch, freq+1};
+        }
+
+        sort(v.begin(), v.end(), [](const auto& p1, const auto& p2){
+            return p1.second>p2.second;
+        });
+
+        for(auto p:v){
+            int ch = p.first;
+            int freq = p.second;
+            string temp = string(freq, ch);
+            ans+=temp;
+        }
+
+        return ans;
+    }
+};
