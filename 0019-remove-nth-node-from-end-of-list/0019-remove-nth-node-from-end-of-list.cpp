d59cdf9701bfa7f7981398981ccc93e9301c5e2a
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-24 23:36:44 +0530

    Time: 8 ms (12.51%), Space: 14.7 MB (55.94%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -14,24 +14,24 @@ public:
         if(head->next==NULL){
             return NULL;
         }
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+
         while(n>0){
-            fast = fast->next;
+            fast=fast->next;
             n--;
         }
-        
-        if (fast == NULL) {
+
+        if(fast==NULL){
             return head->next;
         }
-        
+
         while(fast->next!=NULL){
             slow=slow->next;
             fast=fast->next;
         }
-        
+
         slow->next = slow->next->next;
         return head;
     }
