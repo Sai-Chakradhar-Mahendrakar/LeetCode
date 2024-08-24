b58f14846c2da107f7ba3394a65dd1f8704095ae
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-24 12:05:31 +0530

    Time: 8 ms (67.14%), Space: 10.9 MB (53.66%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,22 +9,18 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (head == NULL) {
-            return false;
-        }
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-            
-            if (slow == fast) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(fast and fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+
+            if(slow==fast){
                 return true;
             }
         }
-        
+
         return false;
     }
 };
