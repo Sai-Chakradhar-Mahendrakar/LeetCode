36cbfab449dd7c5f0b2626c17a3680b170b6da8e
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-27 16:32:28 +0530

    Time: 0 ms (100%), Space: 7.7 MB (34.43%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int differentBits = start^goal;
+        return countSetBits(differentBits);
+    }
+private:
+    int countSetBits(int n){
+        int ans=0;
+        while(n){
+            ans++;
+            n=n&(n-1);
+        }
+        return ans;
+    }
+};
