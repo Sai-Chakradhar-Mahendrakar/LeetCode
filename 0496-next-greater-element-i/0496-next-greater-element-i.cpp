c9c007aad66f494f20f2aae41169ba8a97def2ee
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-30 09:57:20 +0530

    Time: 5 ms (45.55%), Space: 12.3 MB (35.69%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         stack<int> st;
         unordered_map<int, int> mp;
         int n=nums2.size();
