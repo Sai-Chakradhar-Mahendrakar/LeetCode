71813b61a516a7548b17e33586c2dc48548702f2
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-01 12:02:13 +0530

    Time: 14 ms (54.7%), Space: 13.1 MB (89.36%) - LeetHub

---
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        stack<int> st;
+        int n = nums.size();
+        long long totalMax=0, totalMin=0;
+
+        // totalMax
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || nums[st.top()]<nums[i])){
+                int left = st.top();
+                st.pop();
+                int right = st.empty() ? -1 : st.top();
+                totalMax += (long long)nums[left]*(i-left)*(left-right);
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            st.pop();
+        }
+
+        //totalMin
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || nums[st.top()]>nums[i])){
+                int left = st.top();
+                st.pop();
+                int right = st.empty() ? -1 : st.top();
+                totalMin += (long long)nums[left]*(i-left)*(left-right);
+            }
+            st.push(i);
+        }
+
+        return totalMax - totalMin;
+    }
+};
