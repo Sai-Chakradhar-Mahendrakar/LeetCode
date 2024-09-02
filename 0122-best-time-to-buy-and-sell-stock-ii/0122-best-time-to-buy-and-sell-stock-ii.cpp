ef21ee97c43f2707fa3efe8b989d7a72bc9e1936
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-02 10:33:53 +0530

    Time: 4 ms (71.47%), Space: 15.8 MB (72.78%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
-    int dp(vector<int>& prices, bool buy, int idx){
-        if(idx==prices.size()){
-            return 0;
-        }
+    // vector<vector<int>> memo;
+    // int dp(vector<int>& prices, bool buy, int idx){
+    //     if(idx==prices.size()){
+    //         return 0;
+    //     }
 
-        int &ans = memo[idx][buy];
-        if(ans!=-1){
-            return ans;
-        }
+    //     int &ans = memo[idx][buy];
+    //     if(ans!=-1){
+    //         return ans;
+    //     }
 
-        // Exclude
-        ans = dp(prices, buy, idx+1);
+    //     // Exclude
+    //     ans = dp(prices, buy, idx+1);
 
-        // Include
-        if(buy){
-            // Buy
-            ans = max(ans, -prices[idx]+dp(prices, false, idx+1));
-        }else{
-            // Sell
-            ans = max(ans, prices[idx]+dp(prices, true, idx+1));
-        }
+    //     // Include
+    //     if(buy){
+    //         // Buy
+    //         ans = max(ans, -prices[idx]+dp(prices, false, idx+1));
+    //     }else{
+    //         // Sell
+    //         ans = max(ans, prices[idx]+dp(prices, true, idx+1));
+    //     }
 
-        return ans;
-    }
+    //     return ans;
+    // }
     int maxProfit(vector<int>& prices) {
         // memo.resize(prices.size(), vector<int>(2,-1));
         // return dp(prices, true, 0);
         int max=0;
         int start = prices[0];
         for(int i=1;i<prices.size();i++){
-            if(start<prices[i]){
-                max += prices[i] - start;
+            if(prices[i-1]<prices[i]){
+                max += prices[i] - prices[i-1];
             }
-            start = prices[i];
+            // start = prices[i];
         }
         return max;
     }
