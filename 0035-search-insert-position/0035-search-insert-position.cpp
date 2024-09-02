973dafbdc4a0b349388a242c3b1e9ca3bcd874db
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-02 21:25:56 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.46%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        // return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return lowerBound(nums, target);
+    }
+private:
+    int lowerBound(vector<int>& nums, int x){
+        int s=0;
+        int e=nums.size()-1;
+        int ans=nums.size();
+
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(nums[mid]>=x){
+                ans=mid;
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return ans;
     }
 };
