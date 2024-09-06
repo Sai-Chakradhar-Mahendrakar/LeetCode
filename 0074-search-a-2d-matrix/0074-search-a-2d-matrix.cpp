175199eb80c3a81e3830f4493d49899dc47c1124
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-06 22:46:30 +0530

    Time: 0 ms (100%), Space: 12.1 MB (87.56%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // if (matrix.empty() || matrix[0].empty()){
-        //     return false;
-        // }
-
-        // int i=0;
-        // int s = 0;
-        // int e = matrix[0].size()-1;
-        
-        // while(i<matrix.size()){
-        //     while(s<=e){
-        //         int mid = (s+e)/2;
-        //         if(matrix[i][mid]==target){
-        //             return true;
-        //         }else if(matrix[i][mid]>target){
-        //             e = mid-1;
-        //         }else{
-        //             s = mid+1;
-        //         }
-        //     }
-        //     i++;
-        //     s = 0;
-        //     e = matrix[0].size()-1;
-        // }
-        // return false;
-
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int s = 0;
-        int e = n*m-1;
+        int r=matrix.size();
+        int c=matrix[0].size();
+        int s=0;
+        int e=r*c-1;
 
         while(s<=e){
             int mid=(s+e)/2;
-            int r=mid/m;
-            int c=mid%m;
-            if(matrix[r][c]==target){
+            // Flatten 
+            int row=mid/c;
+            int col=mid%c;
+
+            if(matrix[row][col]==target){
                 return true;
-            }else if(matrix[r][c]>target){
+            }
+            else if(matrix[row][col]>target){
                 e=mid-1;
             }
             else{
