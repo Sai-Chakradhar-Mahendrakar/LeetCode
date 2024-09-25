d1f02d8d6a8339635e8de0f06767c5d4349444ab
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-25 08:25:16 +0530

    Time: 12 ms (86.83%), Space: 18.4 MB (49.72%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,7 +10,23 @@
  */
 class Solution {
 public:
-    ListNode* MergeLists(ListNode* a, ListNode* b){
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k=lists.size();
+        if(k==0){
+            return NULL;
+        }
+
+        while(k>1){
+            for(int i=0;i<k/2;i++){
+                lists[i]=mergeLists(lists[i], lists[k-i-1]);
+            }
+            k=(k+1)/2;
+        }
+
+        return lists.front();
+    }
+private:
+    ListNode* mergeLists(ListNode* a, ListNode* b){
         if(a==NULL){
             return b;
         }
@@ -19,30 +35,15 @@ public:
             return a;
         }
 
-        ListNode* c = NULL;
-        if(a->val<=b->val){
-            c = a;
-            c->next = MergeLists(a->next,b);
-        }else{
-            c = b;
-            c->next = MergeLists(a,b->next);
+        ListNode* c=NULL;
+        if(a->val<b->val){
+            c=a;
+            c->next=mergeLists(a->next, b);
         }
-
+        else{
+            c=b;
+            c->next=mergeLists(a,b->next);
+        }
         return c;
-    }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = lists.size();
-        if(n==0){
-            return NULL;
-        }
-
-        while(n>1){
-            for(int i=0;i<n/2;i++){
-                lists[i] = MergeLists(lists[i],lists[n-i-1]);
-            }
-            n = (n+1)/2;
-        }
-
-        return lists.front();
     }
 };
