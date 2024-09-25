cc4f0ce2101a5ca14fd2e145bf38d6c7476824e2
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-25 21:44:49 +0530

    Time: 41 ms (94.55%), Space: 38.1 MB (98.94%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int freq[26]={0};
+        for(auto x:tasks){
+            freq[x-'A']++;
+        }
+        sort(begin(freq), end(freq));
+        int chunk=freq[25]-1;
+        int idle=chunk*n;
+
+        for(int i=24;i>=0;i--){
+            idle-=min(chunk, freq[i]);
+        }
+
+        return idle<0?tasks.size():tasks.size()+idle;
+    }
+};
