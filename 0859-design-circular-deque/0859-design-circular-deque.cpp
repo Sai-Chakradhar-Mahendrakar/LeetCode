2c134af49716739ebef7d2edbdf9d722a9e9dab8
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-28 15:45:00 +0530

    Time: 24 ms (29.32%), Space: 22.7 MB (84.26%) - LeetHub

---
+++ 0859-design-circular-deque/0859-design-circular-deque.cpp
@@ -0,0 +1,70 @@
+class MyCircularDeque {
+public:
+private:
+    std::vector<int> deque;
+    int front, rear, size, capacity;
+
+public:
+    MyCircularDeque(int k) : deque(k, 0), front(0), rear(0), size(0), capacity(k) {}
+
+    bool insertFront(int value) {
+        if (isFull()) return false;
+        front = (front - 1 + capacity) % capacity;
+        deque[front] = value;
+        size++;
+        return true;
+    }
+
+    bool insertLast(int value) {
+        if (isFull()) return false;
+        deque[rear] = value;
+        rear = (rear + 1) % capacity;
+        size++;
+        return true;
+    }
+
+    bool deleteFront() {
+        if (isEmpty()) return false;
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+
+    bool deleteLast() {
+        if (isEmpty()) return false;
+        rear = (rear - 1 + capacity) % capacity;
+        size--;
+        return true;
+    }
+
+    int getFront() {
+        if (isEmpty()) return -1;
+        return deque[front];
+    }
+
+    int getRear() {
+        if (isEmpty()) return -1;
+        return deque[(rear - 1 + capacity) % capacity];
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    bool isFull() {
+        return size == capacity;
+    }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
