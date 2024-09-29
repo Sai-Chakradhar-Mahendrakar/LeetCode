1a6761f3be55238ceee1e651fce9c7448bf0eb21
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-29 22:10:50 +0530

    Time: 0 ms (100%), Space: 12.1 MB (41.02%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,18 +12,18 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL and q==NULL){
+        if(p==NULL && q==NULL){
             return true;
         }
 
-        if(p==NULL or q==NULL){
+        if(p==NULL || q==NULL){
             return false;
         }
 
-        if(p->val != q->val){
+        if(p->val!=q->val){
             return false;
         }
 
-        return isSameTree(p->left,q->left) and isSameTree(p->right,q->right);
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
