1bff464145bef0bf29e3c20aa4af0e92cf3d0aef
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-10-07 22:29:01 +0530

    Time: 64 ms (41.83%), Space: 61.8 MB (26.91%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(0);
         int l=0;
         int r=height.size()-1;
         int area=0;
