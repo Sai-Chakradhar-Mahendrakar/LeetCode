ba9463cdd012dde3930e2c564e564fdafe8d1d8f
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-10-16 07:24:15 +0530

    Time: 8 ms (85.26%), Space: 11.1 MB (80.08%) - LeetHub

--- 2601-number-of-great-partitions/2601-number-of-great-partitions.cpp
+++ 2601-number-of-great-partitions/2601-number-of-great-partitions.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int countPartitions(vector<int>& nums, int k) {
-        long mod=1e9+7,total=0,res=1;
-        vector<long>dp(k);
-        dp[0]=1;
-        for(auto x:nums){
-            for(int i=k-1-x;i>=0;i--){
-                dp[i+x]=(dp[i+x]+dp[i])%mod;
-            }
-            res=res*2%mod;
-            total+=x;
-        }
-
-        for(int i=0;i<k;i++){
-            res-=total-i<k?dp[i]:dp[i]*2;
-        }
-
-        return (res%mod+mod)%mod;
+        long mod=1e9+7, total=0, res=1;
+	    vector<int> dp(k);
+	    dp[0]=1;
+	    for(auto x:nums){
+	    	for(int i=k-1-x;i>=0;i--){
+	    		dp[i+x] = (dp[i]+dp[i+x])%mod;
+	    	}
+	    	res=res*2%mod;
+	    	total+=x;
+	    }
+    
+	    for(int i=0;i<k;i++){
+	    	res-=(total-i<k) ? dp[i] : dp[i]*2;
+	    }
+    
+	    return (res%mod+mod)%mod;
     }
 };
