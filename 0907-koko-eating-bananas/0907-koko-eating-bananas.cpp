e86f589e4f019fd6af3c514ba137ee042ae70daa
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-11-14 15:48:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int n=piles.size();
-        int s=1;
+        int s=0;
         int e=*max_element(piles.begin(), piles.end());
-
         int ans=e;
+
         while(s<=e){
             int mid=(s+e)/2;
             long long hrs=0;
             for(int i=0;i<n;i++){
-                hrs+=ceil((double)piles[i]/mid);
-                if (hrs > h) break; 
+                hrs += ceil((double)piles[i]/mid);
+                if(hrs>h){
+                    break;
+                }
             }
 
             if(hrs>h){
@@ -19,7 +21,7 @@ public:
             }
             else{
                 ans=mid;
-                e=mid-1;;
+                e=mid-1;
             }
         }
         return ans;
