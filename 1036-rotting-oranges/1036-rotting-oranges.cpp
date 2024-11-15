e439cbcdc57e6d380e555f0e70c1778dac4bd30d
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-11-15 16:23:15 +0530

    Time: 0 ms (100%), Space: 15.9 MB (99.5%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,65 +1,41 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
+        int r=grid.size();
+        int c=grid[0].size();
 
-        return bfs(grid, r, c);
-    }
-
-private:
-    int dx[4] = {1, -1, 0, 0};
-    int dy[4] = {0, 0, 1, -1};
-
-    int bfs(vector<vector<int>>& grid, int r, int c) {
-        queue<pair<int, int>> q;
-        int ans = -1;
-        int fresh = 0;
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
-                }
-            }
-        }
-
-        vector<vector<int>> dist(r, vector<int>(c,0));
-        if(fresh==0){
-            return 0;
-        }
-
-        while(!q.empty()){
-            pair<int, int> pr = q.front();
-            int x = pr.first;
-            int y = pr.second;
-
-            q.pop();
-
-            for(int i=0;i<4;i++){
-                int xx = x+dx[i];
-                int yy = y+dy[i];
-
-                if(xx>=0 and xx<r and yy>=0 and yy<c and grid[xx][yy]==1){
-                    q.push({xx, yy});
-                    grid[xx][yy]=2;
-                    dist[xx][yy]= dist[x][y]+1;
-                }
-            }
-        }
-        int maxi=-1;
-        
-        for(int i=0;i<r;i++)
-        {
+        for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j]==1)
-                    return -1;
-                maxi = max(maxi , dist[i][j]);
+                if(grid[i][j]==2){
+                    dfs(grid, i, j, r, c, 2);
+                }
             }
-            
         }
-        return maxi;
+
+        int ans=INT_MIN;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+                ans=max(ans, grid[i][j]);
+            }
+        }
+        return ans<=0? 0 : ans-2;
+    }
+private:
+    int dx[4] = {0, 0, -1, 1};
+    int dy[4] = {1, -1, 0, 0};
+    void dfs(vector<vector<int>>& grid, int i, int j, int r, int c, int min){
+        if(i<0 || i==r || j<0 || j==c || grid[i][j]==0 || 
+            (grid[i][j]<min && grid[i][j]>1)){
+                return;
+        }
+        grid[i][j]=min;
+        for(int k=0;k<4;k++){
+            int nx=i+dx[k];
+            int ny=j+dy[k];
+            dfs(grid, nx, ny, r, c, min+1);
+        }
     }
 };
