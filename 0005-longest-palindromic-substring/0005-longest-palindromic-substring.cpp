a9fe5bcd2423387118b1f827f0c8c9d62cc0b8cf
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-11-17 19:17:09 +0530

    Time: 76 ms (55.42%), Space: 165 MB (27.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-
         int n=s.size();
         if(n==0){
             return "";
