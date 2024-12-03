652aff4ff589c50c5d50671f5439e87f8131ea9d
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-12-03 09:43:46 +0530

    Time: 46 ms (65.95%), Space: 51 MB (13.8%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         unordered_map<int, int> mp;
 
         int sum = 0;
