0b421daea2b9de1af4303f27faf7db1dec2ab112
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-12-03 09:29:34 +0530

    Time: 0 ms (100%), Space: 10.3 MB (6.21%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-
         int r=matrix.size();
         int c=matrix[0].size();
 
@@ -20,10 +16,7 @@ public:
         }
 
         for(int i=0;i<r;i++){
-            for(int j=0;j<(c/2);j++){
-                swap(matrix[i][j], matrix[i][c-j-1]);
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
-
     }
 };
