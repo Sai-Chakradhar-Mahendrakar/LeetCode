2b429c795604967148417aee89f2763a8c3bf1de
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-12-03 09:07:47 +0530

    Time: 0 ms (100%), Space: 15.8 MB (5.11%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,13 +5,15 @@ public:
         int i=n-2;
         while(i>=0 and nums[i]>=nums[i+1]){
             i--;
-        } 
+        }
+        cout<<i<<endl; 
         if(i>=0){
             int j=n-1;
             while(nums[j]<=nums[i]){
                 j--;
             }
 
+            cout<<j<<endl;
             swap(nums[i], nums[j]);
         }
         reverse(nums.begin()+i+1, nums.end());
