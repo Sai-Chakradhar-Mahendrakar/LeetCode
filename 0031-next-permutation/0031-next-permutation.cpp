708e4428d3685da373d7cd75a6431d95f0afc535
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-12-03 08:59:11 +0530

    Time: 0 ms (100%), Space: 15.5 MB (7.08%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         int n=nums.size();
         int i=n-2;
         while(i>=0 and nums[i]>=nums[i+1]){
