40e8331c1f2f4b3a8385e74e3490290c86d36247
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-12-03 16:05:28 +0530

    Time: 0 ms (100%), Space: 8.5 MB (20.66%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,13 +4,11 @@ public:
         if(n==0){
             return 1;
         }
-
         long long N = n;
         while(N<0){
             x = 1/x;
             N = -N;
         }
-
         double res=1;
         while(N){
             if(N&1){
