64f87f3afe0cf9e4ae3344afca901017a3f2398d
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-12-03 13:51:04 +0530

    Time: 1 ms (62.54%), Space: 9.4 MB (71.26%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-
         if(s.size()!=t.size()){
             return false;
         }
