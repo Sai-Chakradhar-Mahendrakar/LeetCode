f38df0c650dc408964adf0d9031747d27eecb54c
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-12-03 23:34:18 +0530

    Time: 0 ms (100%), Space: 50.3 MB (10.65%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         int n=mat.size();
         int m=mat[0].size();
 
