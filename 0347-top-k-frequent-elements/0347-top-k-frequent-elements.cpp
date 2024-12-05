e8e2e84fd07c22061a4ed0224057a4b27eaa5c8e
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-12-05 21:53:11 +0530

    Time: 0 ms (100%), Space: 19.8 MB (7.15%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,12 +6,10 @@ public:
         for(auto x:nums){
             mp[x]++;
         }
-
         vector<vector<int>> freq(nums.size()+1);
         for(auto x:mp){
             freq[x.second].push_back(x.first);
         }
-
         vector<int> res;
         for(int i=freq.size()-1;i>=0;i--){
             for(int num:freq[i]){
@@ -21,7 +19,6 @@ public:
                 }
             }
         }
-
         return {};
     }
 };
