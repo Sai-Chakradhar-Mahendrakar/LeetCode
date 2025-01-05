3a74820b0ec04608b199d332ce63edc288f66313
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2025-01-05 20:35:44 +0530

    Time: 4 ms (76.8%), Space: 97.6 MB (84.6%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.size();
+        vector<int> shift(n + 1, 0);
+
+        // Process the shifts
+        for (auto& shiftOp : shifts) {
+            int start = shiftOp[0], end = shiftOp[1], direction = shiftOp[2];
+            shift[start] += (direction == 1 ? 1 : -1);
+            shift[end + 1] -= (direction == 1 ? 1 : -1);
+        }
+
+        int currentShift = 0;
+        for (int i = 0; i < n; ++i) {
+            currentShift += shift[i];
+            shift[i] = currentShift;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            int netShift = (shift[i] % 26 + 26) % 26;
+            s[i] = 'a' + (s[i] - 'a' + netShift) % 26;
+        }
+
+        return s;
+    }
+};
