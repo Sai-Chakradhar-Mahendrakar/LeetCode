97c153207dd063c38ce617c83e05a53e79ad501b
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2025-09-02 19:20:20 +0530

    Time: 13 ms (30.86%), Space: 7.8 MB (63.99%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-
-        int a=0, b=1, c;
-        for(int i=2;i<=n;i++){
-            c = a+b;
-            a = b;
-            b = c;
-        }
-        
-        return c;
+        if(n<=1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };
