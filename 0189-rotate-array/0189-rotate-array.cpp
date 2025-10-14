111ceb87aa56af853c18eabb79908d0e107a97b7
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2025-10-14 08:59:03 +0530

    Time: 0 ms (100%), Space: 29.6 MB (60.82%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        k = k % n;
+        k=k%n;
         reverse(nums.begin(), nums.begin()+n-k);
         reverse(nums.begin()+n-k, nums.end());
         reverse(nums.begin(), nums.end());
