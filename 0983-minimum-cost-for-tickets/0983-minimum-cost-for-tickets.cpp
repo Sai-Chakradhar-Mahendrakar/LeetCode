0c5020427b9862ed7d4e01f15d48e339227c573d
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2025-12-25 20:56:45 +0530

    Time: 0 ms (100%), Space: 13.3 MB (48.78%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int lastDay = days.back();
+        vector<int> dp(lastDay+1, 0);
+
+        unordered_set<int> travel(days.begin(), days.end());
+
+        for(int i=1;i<=lastDay;i++){
+            if(travel.find(i) == travel.end()){
+                dp[i]=dp[i-1];
+                continue;
+            }
+
+            dp[i] = min({
+                dp[i-1]+costs[0],
+                dp[max(0, i-7)]+costs[1],
+                dp[max(0, i-30)]+costs[2]
+            });
+        }
+
+        return dp[lastDay];
+    }
+};
