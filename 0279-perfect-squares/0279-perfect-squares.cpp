f1d2b61d0148e63f625e685a00af88c9089342a5
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2026-01-08 09:24:52 +0530

    Time: 35 ms (88.44%), Space: 13.2 MB (67.3%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> dp(n + 1, INT_MAX);
-        dp[0] = 0;
+        vector<int> dp(n+1, INT_MAX);
+        dp[0]=0;
 
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j * j <= i; ++j) {
-                dp[i] = min(dp[i], dp[i - j * j] + 1);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i] = min(dp[i], dp[i-j*j]+1);
             }
-        }
-
-        for(auto x:dp){
-            cout<<x<<" ";
         }
 
         return dp[n];
