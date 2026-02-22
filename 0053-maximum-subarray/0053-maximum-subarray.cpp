f75c325df10b518972aa4b4541d0c9d0b9e88fce
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2026-02-22 20:54:22 +0530

    Time: 0 ms (100%), Space: 71.6 MB (97.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans = nums[0];
-        int cur = nums[0];
-        for (auto i = 1; i < nums.size(); i++) {
-            cur = max(nums[i], cur + nums[i]);
-            ans = max(ans, cur);
+        int currSum=nums[0];
+        int res=nums[0];
+        for(int i=1;i<nums.size();i++){
+            currSum=max(nums[i], currSum+nums[i]);
+            res=max(currSum, res);
         }
-        return ans;
+        return res;
     }
 };
-
-static const auto _____ = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    return nullptr;
-}();
